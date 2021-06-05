cfe6e173fd0d847a70e6860c0a496d36c5737dc2
fudaolai <fudaolai_java@163.com>
2021-06-05 11:50:15 +0800

    1

---
+++ src/main/java/com/example/neverstop/NeverstopApplication.java
@@ -0,0 +1,13 @@
+package com.example.neverstop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeverstopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeverstopApplication.class, args);
+	}
+
+}
