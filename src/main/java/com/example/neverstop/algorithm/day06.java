71d1147f7a7ae7334cedcfb33a46255df1fbfe30
fudaolai <fudaolai_java@163.com>
2021-06-23 21:56:02 +0800

    algorithm 06

---
+++ src/main/java/com/example/neverstop/algorithm/day06.java
@@ -0,0 +1,35 @@
+package com.example.neverstop.algorithm;
+
+public class day06 {
+
+
+    /**
+     * 实现字符串反转
+     * ps：String str = "abc123"   输出 ： 321cba
+     */
+
+
+    public static void main(String[] args) {
+
+        //第一种：使用reverse
+        String str = "abc123";
+//
+//        StringBuffer sb = new StringBuffer(str);
+//
+//        StringBuffer reverse = sb.reverse();
+//        System.out.println(reverse);
+
+
+        //第二种方式
+        char[] chars = str.toCharArray();
+
+        String res = "";
+        for (int i = str.length() - 1; i >=0 ;i --){
+
+            res += chars[i];
+        }
+        System.out.println(res);
+
+    }
+
+}
